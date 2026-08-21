9a5d4c6108a09c2991e0be40df7b7ee5ad7bbd35
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2026-08-20 18:08:14 -0600

    chore: topbar population from theme options over theme settings submodule

--- template-parts/header/topbar.php
+++ template-parts/header/topbar.php
@@ -1,3 +1,26 @@
+<?php
+
+$phone = ccluster_get_theme_option('phone');
+$email = ccluster_get_theme_option('email');
+
+?>
+
 <div id="site-topbar">
-    Topbar
+
+    <?php if ($phone) : ?>
+
+        <span>
+            <?php echo esc_html($phone); ?>
+        </span>
+
+    <?php endif; ?>
+
+    <?php if ($email) : ?>
+
+        <span>
+            <?php echo esc_html($email); ?>
+        </span>
+
+    <?php endif; ?>
+
 </div>
