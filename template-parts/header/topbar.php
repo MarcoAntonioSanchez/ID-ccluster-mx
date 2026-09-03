abacaa0c4a05e135c2947419bc5ee9db9e4ac53d
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2026-09-03 11:19:02 -0600

    update: topbar v4

--- template-parts/header/topbar.php
+++ template-parts/header/topbar.php
@@ -12,7 +12,7 @@ $address = ccluster_get_theme_option('address');
 ?>
 <div
     id="site-topbar"
-    class="border-1 border-b-slate-400">
+    class="border-1 border-b-slate-200">
     <div
         class="mx-auto flex max-w-7xl items-center justify-between px-6 py-2">
         <div class="flex items-center gap-6">
