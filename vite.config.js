762030a8caa3107a0c380788688748dcf65505e6
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2026-09-07 18:51:29 -0600

    fix: vite fonts path correction for wp theme

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: "./",
+
   plugins: [tailwindcss()],
 
   build: {
     manifest: true,
+
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/js/main.js"),
