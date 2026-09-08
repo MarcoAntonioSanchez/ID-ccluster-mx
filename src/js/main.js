8019c5a06bd63dc18b06cf334ed40ac8c70fec9a
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2026-09-08 17:00:16 -0600

    update: yt and ig simple-icons render

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,22 @@
 import { createIcons, Check, Menu, X, Phone, Mail, MapPin } from "lucide";
-import { siFacebook } from "simple-icons";
+import { siFacebook, siInstagram, siYoutube } from "simple-icons";
 
 document.addEventListener("DOMContentLoaded", () => {
   const facebookIcon = siFacebook.svg;
   const facebookElement = document.querySelector('[data-social="facebook"]');
+  const instagramIcon = siInstagram.svg;
+  const instagramElement = document.querySelector('[data-social="instagram"]');
+  const youtubeIcon = siYoutube.svg;
+  const youtubeElement = document.querySelector('[data-social="youtube"]');
 
   if (facebookElement) {
     facebookElement.innerHTML = facebookIcon;
+  }
+  if (instagramElement) {
+    instagramElement.innerHTML = instagramIcon;
+  }
+  if (youtubeElement) {
+    youtubeElement.innerHTML = youtubeIcon;
   }
 
   createIcons({
