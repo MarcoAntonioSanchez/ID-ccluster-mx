61ce5cc8ac9b49f19cbe9296489d4dc64018bfe0
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2026-09-07 19:02:39 -0600

    update: hero title lettering wieight v2

--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -93,7 +93,7 @@ $hero_background = $hero_background_id
                 </div>
             <?php endif; ?>
             <?php if ($hero_title) : ?>
-                <h1 class="max-w-2xl text-4xl capitalize leading-tight md:text-6xl font-heading">
+                <h1 class="max-w-2xl text-7xl capitalize leading-tight md:text-7xl font-heading">
                     <?php echo esc_html($hero_title); ?>
                 </h1>
             <?php endif; ?>
