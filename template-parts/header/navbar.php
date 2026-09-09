dd193e7b5ac4d96a700fa44b1c42d606c8969bd6
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2026-09-09 15:26:08 -0600

    fix: navbar cta icon v5

--- template-parts/header/navbar.php
+++ template-parts/header/navbar.php
@@ -60,7 +60,7 @@ $cta_url = ccluster_get_theme_option(
                 <a
                     href="<?php echo esc_url($cta_url); ?>"
                     class="flex gap-[5px] bg-[#0F143A] px-6 py-3 text-sm font-semibold text-white capitalize duration-300 ease-in-out hover:scale-[1.05] hover:shadow-md">
-                    <span class="flex-1 self-center ccluster-cta__icon">
+                    <span class="self-center ccluster-cta__icon">
                         <i data-lucide="phone-call"></i>
                     </span>
                     <span class="flex-1">
