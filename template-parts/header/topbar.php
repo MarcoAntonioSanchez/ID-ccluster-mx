806c51b153393a684d3bf4d81605cc53b0e25c55
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2026-09-08 18:39:16 -0600

    test: hover state on topbar links v5

--- template-parts/header/topbar.php
+++ template-parts/header/topbar.php
@@ -24,7 +24,7 @@ $address = ccluster_get_theme_option('address');
                     </span>
                     <a
                         href="mailto:<?php echo esc_attr($email); ?>"
-                        class="text-xs flex-1 duration-300 ease-in-out hover:p-left-[5px] hover:scale-[1.01] hover:font-medium">
+                        class="text-xs flex-1 duration-300 ease-in-out hover:scale-[1.01] hover:border-1 hover:border-(--primary)">
                         <?php echo esc_html($email); ?>
                     </a>
                 </div>
