7fed195d5aa949b712454eddf05c27f8b9e53280
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2026-09-09 18:23:59 -0600

    update: our story frontend base layout

---
+++ template-parts/home/nuestra-historia.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<section class="ccluster-nuestra-historia">
+    <!-- TOP ROW -->
+    <div class="ccluster-nuestra-historia__timeline">
+    </div>
+    <!-- BOTTOM ROW -->
+    <div class="ccluster-nuestra-historia__stats">
+    </div>
+</section>
