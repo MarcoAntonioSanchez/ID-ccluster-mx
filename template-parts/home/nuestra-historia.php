d7a6595e3a30eee3f9c691a59cd0f6013e2d966a
Marco A. Sánchez <marco.a.sanchez.dev@gmail.com>
2026-09-09 18:31:02 -0600

    update: our story top row badge and title

--- template-parts/home/nuestra-historia.php
+++ template-parts/home/nuestra-historia.php
@@ -6,22 +6,57 @@ $background_id = absint(
         true
     )
 );
-
 $background_url = $background_id
     ? wp_get_attachment_image_url(
         $background_id,
         'full'
     )
     : '';
+
+$badge_icon_id = absint(
+    get_post_meta(
+        get_the_ID(),
+        'historia_badge_icon',
+        true
+    )
+);
+$badge_text = get_post_meta(
+    get_the_ID(),
+    'historia_badge_text',
+    true
+);
 ?>
 <section class="ccluster-nuestra-historia">
     <!-- TOP ROW -->
-    <div class="ccluster-nuestra-historia__timeline">
-        <div
-            class="ccluster-nuestra-historia__timeline"
-            <?php if ($background_url) : ?>
-            style="background-image: url('<?php echo esc_url($background_url); ?>');"
-            <?php endif; ?>>
+    <div
+        class="ccluster-nuestra-historia__timeline"
+        <?php if ($background_url) : ?>
+        style="background-image: url('<?php echo esc_url($background_url); ?>');"
+        <?php endif; ?>>
+        <div class="ccluster-nuestra-historia__header">
+
+            <?php if ($badge_icon_id) : ?>
+                <?php
+                echo wp_get_attachment_image(
+                    $badge_icon_id,
+                    'thumbnail',
+                    false,
+                    [
+                        'class' => 'ccluster-nuestra-historia__badge-icon',
+                        'alt'   => '',
+                    ]
+                );
+                ?>
+            <?php endif; ?>
+
+            <span class="ccluster-nuestra-historia__badge-separator"></span>
+
+            <?php if ($badge_text) : ?>
+                <span class="ccluster-nuestra-historia__badge-text">
+                    <?php echo esc_html($badge_text); ?>
+                </span>
+            <?php endif; ?>
+
         </div>
     </div>
     <!-- BOTTOM ROW -->
